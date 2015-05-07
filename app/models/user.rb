93c27578fd1aa8257ee4e75f954d0f1b4d1f8c6e
ougarcia <oz.ulysses@gmail.com>
2015-05-07 15:00:08 -0700

    Start the bonust task

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,31 @@ class User < ActiveRecord::Base
     primary_key: :id
   )
 
+  def completed_polls
+    #start by getting the questions per poll
+    Poll.find_by_sql(<<-SQL, id)
+      SELECT
+        polls.*, COUNT(questions.id)
+      FROM
+        polls
+      JOIN
+        questions ON poll.id = questions.poll_id
+      JOIN
+        answer_choices ON questions.id = answer_choices.question_id
+      LEFT OUTER JOIN(
+        SELECT
+          *
+        FROM
+          responses
+        WHERE
+          user_id = ?
+        ) AS sub_responses ON sub_responses.user_id = user.id
+      GROUP BY
+        polls.id
+      HAVING
+        COUNT(questions.id) = COUNT(responses.id)
+    SQL
+
+  end
+
 end
