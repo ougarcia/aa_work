d0287ef0c7e57c1a9139167b347d3d8f03879a9c
ougarcia <oz.ulysses@gmail.com>
2015-05-10 20:29:05 -0700

    add _form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
 
   def index
     @articles = Article.all
@@ -6,6 +7,34 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save!
+
+    redirect_to article_url(@article)
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def destroy
+    Article.find(params[:id]).destroy
+    
+    redirect_to articles_url
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    redirect_to article_url(@article)
   end
 
 end
