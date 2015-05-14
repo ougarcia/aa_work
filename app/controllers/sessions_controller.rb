7dd962d6538f33a0e901f340605318fb83227651
ougarcia <oz.ulysses@gmail.com>
2015-05-14 10:33:05 -0700

    First commit for music app, create basic user model

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,34 @@
+class SessionsController < ApplicationController
+
+  def new
+    if logged_in?
+      redirect_to user_url(current_user)
+    else
+      @user = User.new
+    end
+  end
+  
+  def create
+    @user = User.find_by_credentials(user_params[:email], user_params[:password])
+    if @user
+      log_in_user!(@user)
+      redirect_to user_url(@user)
+    else
+      @user = User.new(user_params)
+      render :new
+    end
+  end
+
+  def destroy
+    @user = User.find_by(session_token: session[:session_token])
+    log_out_user!(@user)
+    redirect_to new_session_url
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def new
+    if logged_in?
+      redirect_to user_url(current_user)
+    else
+      @user = User.new
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:session_token] = @user.session_token
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
