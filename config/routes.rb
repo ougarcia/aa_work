e2aae69a5c4b49ba42e9212cbf6b5d2f607fd367
ougarcia <oz.ulysses@gmail.com>
2015-05-14 12:57:39 -0700

    Create and complete bands controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
   resource :session, only: [:new, :create, :destroy]
+
+  resources :bands do
+    resources :albums, only: :new
+  end
+
+  resources :albums, except: :new do
+    resources :tracks, only: :new
+  end
+
+  resources :tracks, except: :new
+
 end
