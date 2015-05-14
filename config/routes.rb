02a607331ea2ba499379f8b82186a6eb87754e50
ougarcia <oz.ulysses@gmail.com>
2015-05-14 14:08:28 -0700

    Create and complete albums controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,10 @@ Rails.application.routes.draw do
     resources :albums, only: :new
   end
 
-  resources :albums, except: :new do
+  resources :albums, except: [:new, :index] do
     resources :tracks, only: :new
   end
 
-  resources :tracks, except: :new
+  resources :tracks, except: [:new, :index]
 
 end
